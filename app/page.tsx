9baebc13362a13bb5c73deddc4acd7ac539d8725
Piotr <hunter976@gmail.com>
2026-02-16 04:06:02 +0100

    first part of calculator settings upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,127 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="w-full flex-1 bg-black/70 text-white p-6">
+      <div className="max-w-100 mx-auto flex flex-col gap-10">
+        <header className="flex flex-row items-center justify-between">
+          <div className="rounded-md overflow-hidden">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/mark.jpg"
+              alt="Mark of Sergaras Icon"
+              width={56}
+              height={56}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          </div>
+          <h1 className="text-center text-3xl">
+            <span className="text-my-lavender">Aldor</span> |{' '}
+            <span className="text-my-turquoise">Scryer</span> <br />
+            <span className="text-xl font-light">Reputation Calculator</span>
+          </h1>
+          <div className="rounded-md overflow-hidden">
+            <Image
+              src="/signet.jpg"
+              alt="Mark of Sergaras Icon"
+              width={56}
+              height={56}
+            />
+          </div>
+        </header>
+        <main className="flex flex-col items-center gap-4">
+          <h2 className="text-center text-sm font-bold">
+            Calculate how many marks or signets you need to hit your goal
+          </h2>
+          <div className="w-full border-2 bg-my-obsidian/90 rounded-md border-my-violet text-my-sand">
+            <div className="border-b-2 border-my-violet p-5 font-bold flex gap-2 items-center">
+              <Image
+                src="/calc.svg"
+                width={800}
+                height={800}
+                alt="Calculator icon"
+                className="w-7"
+              />
+              <h3 className="text-xl text-white">Calculator Settings</h3>
+            </div>
+            <div className="flex flex-col gap-8 p-5">
+              <div className="flex flex-col gap-4">
+                <h3 className="font-bold text-sm text-white">
+                  Current reputation
+                </h3>
+                <div className="flex flex-row gap-4">
+                  <div className="flex-1">
+                    <button className="p-2 rounded-md border w-full flex flex-row justify-between items-center border-my-violet text-my-lavender ">
+                      <span>Friendly</span> <span>v</span>
+                    </button>
+                  </div>
+                  <div className="basis-1/4">
+                    <input
+                      type="number"
+                      name="rep"
+                      id="rep"
+                      className="w-full h-full border border-my-violet text-my-lavender px-2 rounded-md"
+                      placeholder="0"
+                    />
+                  </div>
+                </div>
+                <p className="text-xs -mt-2 text-my-lavender">
+                  Reputation range: 0-6000
+                </p>
+              </div>
+              <div className="flex flex-col gap-4">
+                <h3 className="font-bold text-sm text-white">
+                  Goal Reputation
+                </h3>
+                <div className="flex flex-row gap-4">
+                  <div className="flex-1">
+                    <button className="p-2 rounded-md border w-full flex flex-row justify-between items-center border-my-violet text-my-lavender ">
+                      <span>Friendly</span> <span>v</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <h3 className="font-bold text-sm text-white">
+                  Marks/Signets Price (gold)
+                </h3>
+                <div className="flex flex-row gap-4">
+                  <Image
+                    src="/gold.svg"
+                    width={800}
+                    height={800}
+                    alt="Gold coins incon"
+                    className="w-6"
+                  />
+                  <div className="flex-1">
+                    <input
+                      type="number"
+                      name="rep"
+                      id="rep"
+                      className="w-full h-full border border-my-violet text-my-lavender p-2 rounded-md"
+                      placeholder="0"
+                    />
+                  </div>
+                </div>
+                <p className="text-xs -mt-2 text-my-lavender">
+                  Price in gold (e.g. 2 gold 33 silver = 2,33)
+                </p>
+              </div>
+              <div className="flex flex-row gap-2 items-center">
+                <div>
+                  <div className="p-1 w-10 rounded-2xl bg-my-violet">
+                    <div className="w-4 h-4 bg-my-obsidian rounded-[50%]"></div>
+                  </div>
+                </div>
+                <div className="text-xs text-my-lavender">
+                  <p className="font-bold">Human Racial (Doplomacy)</p>
+                  <p className="text-my-violet">
+                    Humans gain 10% more reputation
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
