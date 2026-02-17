9f944a55c792f1832d5054feb1fc9cf361333763
Piotr <hunter976@gmail.com>
2026-02-17 05:22:17 +0100

    fixed critical bug with rep data

--- data/reputation.ts
+++ data/reputation.ts
@@ -8,7 +8,7 @@ interface ReputationProps {
 export const reputationData = [
   { id: 0, name: 'Neutral', have: 0, need: 0 },
   { id: 1, name: 'Friendly', have: 3000, need: 3000 },
-  { id: 2, name: 'Honored', have: 9000, need: 12000 },
+  { id: 2, name: 'Honored', have: 9000, need: 9000 },
   { id: 3, name: 'Revered', have: 21000, need: 21000 },
   { id: 4, name: 'Exalted', have: 42000, need: 42000 },
 ];
